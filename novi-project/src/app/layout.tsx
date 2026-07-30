efd1071ebb5b0f1fd0e73b991e1baf5aded6e75f
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-07-30 20:34:23 -0300

    main

---
+++ novi-project/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+import { Header } from '../components/header'
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`antialiased`}
+    >
+      <Header/>
+
+      
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ novi-project/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+    <div>
+      <h1>Página HOME</h1>
+    </div>
+  )
+}
